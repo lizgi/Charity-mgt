d4280a4a76035386e19a627edbde0385f8c1e567
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-01-19 15:48:31 +0300

    create signup function

--- authenticationApp/views.py
+++ authenticationApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def signup(request):
+    '''View function to present users with account choices'''
+    title = 'Sign Up'
+    return render(request,'registration/signup.html',{'title': title})
