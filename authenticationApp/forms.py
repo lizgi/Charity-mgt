48477dc8088bb93cffeecd2161fd2ff4f1818af7
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-01-19 15:43:19 +0300

    create donorsignup form

--- authenticationApp/forms.py
+++ authenticationApp/forms.py
@@ -3,3 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 
 from authenticationApp.models import *
+
+
+class DonorSignUp(UserCreationForm):
+    first_name= forms.CharField(label='First Name' ,error_messages={'required': 'Please enter your first name'})
+    last_name= forms.CharField(label='Last Name',error_messages={'required': 'Please enter your last name'})
+    email= forms.EmailField(label='Email Address' ,help_text='Format: 123@gmail.com, 456@yahoo.com',error_messages={'required': 'Please enter your email address'})
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields=['first_name','last_name','username','email','password1','password2']
