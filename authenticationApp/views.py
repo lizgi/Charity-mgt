a4d44cf1a2d58aa13eda6f5069b292fa43fa0eed
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-01-19 16:02:53 +0300

    import settings and forms

--- authenticationApp/views.py
+++ authenticationApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
+from .forms import *
 
 # Create your views here.
 
@@ -8,3 +12,5 @@ def signup(request):
     '''View function to present users with account choices'''
     title = 'Sign Up'
     return render(request,'registration/signup.html',{'title': title})
+
+
