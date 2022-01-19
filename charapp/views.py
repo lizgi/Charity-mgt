fa32c4dd94aa1a75f6557c498cd7d7a7f5a2be1d
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-19 21:56:22 +0300

    add view function

--- charapp/views.py
+++ charapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,HttpResponse
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from . import *
+from datetime import date
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def Index(request):
+    return render(request, 'index.html')
