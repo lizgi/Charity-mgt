3c0690b58de893194cfc37a08bcc7f696a1291ef
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-01-19 15:32:16 +0300

    create User model in models.py

--- authenticationApp/models.py
+++ authenticationApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+#create custoclass User(AbstractUser):
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_author = models.BooleanField(default=False)
