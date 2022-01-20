4fbbbc4668bb2a4c562296cf6adf62e95a1c52d5
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-20 07:08:20 +0300

    update and create forms.py

--- charapp/forms.py
+++ charapp/forms.py
@@ -1,12 +1,9 @@
 from django import forms
-from .models import NGO, donation_request
+from .models import NGO
 
 class NGO_form(forms.ModelForm):
     class Meta:
         model = NGO
         fields = ('ngo_name','domain','head_of_ngo','contactNo','email')
 
-class donation_form(forms.ModelForm):
-    class Meta:
-        model = donation_request
-        fields = ('donation_description','donation_amount')
+
