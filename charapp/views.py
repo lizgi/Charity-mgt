3a61a3bc832281b601ffa1dff967bebe51219de0
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-20 07:58:34 +0300

    update ngo views

--- charapp/views.py
+++ charapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from charapp.forms import NGO_form
 
 # Create your views here.
+
+def ngo(request):
+    if request.method == 'POST':
+        form = NGO_form(request.POST, request.FILES)
+        if form.is_valid():
+            # form.save()
+            return redirect('http://127.0.0.1:8000/')
+    else:
+        form = NGO_form()
+
+    return render(request,"verify.html",{'form':form})
