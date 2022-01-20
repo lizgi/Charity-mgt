565896cc2c19fb7bdb2f90714f231b438a09a8ec
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-20 07:56:04 +0300

    update ngo models

--- charapp/models.py
+++ charapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class NGO(models.Model):
+    ngo_name = models.CharField(max_length=30,blank=True)
+    domain = models.CharField(max_length=20,blank=True)
+    head_of_ngo = models.CharField(max_length=30,blank=True)
+    contactNo = models.CharField(max_length=10,blank=True)
+    email = models.EmailField(blank=True)
