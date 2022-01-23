f69ddb68f5ac8addc6d1092a1eed9c04bdf8e212
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-23 22:26:33 +0300

    add validilation form for profile in views.py

--- charapp/views.py
+++ charapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import  donation_form
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -17,3 +21,27 @@ def donation(request):
 
 def blog(request):
     return render(request, 'blog.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
