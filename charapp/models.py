74d050847c2350664dc9401feee4d5f37d59d455
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-23 22:24:05 +0300

    add save image image in models.py

--- charapp/models.py
+++ charapp/models.py
@@ -6,6 +6,8 @@ from tkinter import CASCADE
 from django.db import models
 from django_currentuser.middleware import get_current_user, get_current_authenticated_user
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
 class donation_request(models.Model):
@@ -25,3 +27,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
