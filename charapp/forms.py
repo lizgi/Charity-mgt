86ce9904d425cd80f46fc91a8cae8861dec2a6f5
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-23 22:22:12 +0300

    add userregistration class in forms.py

--- charapp/forms.py
+++ charapp/forms.py
@@ -1,5 +1,9 @@
 from .models import  donation_request
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 
 
@@ -8,3 +12,24 @@ class donation_form(forms.ModelForm):
     class Meta:
         model = donation_request
         fields = ('donation_description','donation_amount')
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
