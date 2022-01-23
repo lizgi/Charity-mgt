91fb0ec0d3401822db8687976e58347b598eb5dd
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-23 22:20:19 +0300

    add ready method in apps.py

--- charapp/apps.py
+++ charapp/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CharappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'charapp'
+def ready(self):
+        import charapp.signals
