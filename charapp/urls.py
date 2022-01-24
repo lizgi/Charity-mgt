cc6182639eec40a2a924519f392371b08a9c35db
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-24 14:36:57 +0300

    Landing page

--- charapp/urls.py
+++ charapp/urls.py
@@ -7,9 +7,9 @@ from .views import Index
 urlpatterns = [
     path('', Index, name='index'),
     path('donation/', views.donation),
-    path('blog/', views.blog),
-    path('ngo', views.ngo),
-    path('gallery', views.gallery),
+    path('blog/', views.blog, name='blog'),
+    path('ngo/', views.ngo),
+    path('gallery/', views.gallery, name='gallery'),
     path('contact/', views.Contact, name='contactUs'),
 
 ]
