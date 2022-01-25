9b6ad28899a2cc6a672c8aef7b224e58e68a8848
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-25 08:02:54 +0300

    update views.py profile

--- charapp/views.py
+++ charapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileUpdateForm
 from charapp.forms import NGO_form
 
 # Create your views here.
@@ -14,3 +16,27 @@ def ngo(request):
         form = NGO_form()
 
     return render(request,"verify.html",{'form':form})
+
+# @login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profilee.html', context)
