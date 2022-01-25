3f45c1e572c5286aa46cc72f4222b6c97b3bba2a
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-25 19:23:34 +0300

    add ngo form

--- charapp/models.py
+++ charapp/models.py
@@ -49,9 +49,32 @@ class NGO(models.Model):
     head_of_ngo = models.CharField(max_length=30,blank=True)
     contactNo = models.CharField(max_length=10,blank=True)
     email = models.EmailField(blank=True)
-    
+    ngo_current_user = models.CharField(default=0,blank=True,max_length=40)
+
+    def verification_true(self):
+        self.verification_status=True        
+        self.save()
+
+    def verification_false(self):
+        self.verification_status = False
+        self.save()
+
+    def get_user(self,user):
+        self.ngo_user = user
+        self.save()
+
     def __str__(self):
         return self.ngo_name
+
+
+class donation_request(models.Model):
+
+    donation_description = models.TextField(blank=True,default=None)
+    donation_amount = models.CharField(default=None,blank=True,max_length=15)
+    donation_request_user = models.CharField(blank=True,default=get_current_authenticated_user,max_length=30)
+
+    def __str__(self):
+        return self.donation_amount
 
 class donation_request_view(models.Model):
     ngo_name = models.CharField(default=None,max_length=50,blank=False,primary_key=True)
