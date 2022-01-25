2cc05739edb1f6373763e8d1a96c9fb25fb2ee8c
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-25 17:54:09 +0300

    add donation request

--- charapp/models.py
+++ charapp/models.py
@@ -14,10 +14,11 @@ from PIL import Image
 # Create your models here.
 
 class donation_request(models.Model):
-
+    
     donation_description = models.TextField(blank=True,default=0)
     donation_amount = models.CharField(default=None,blank=True,max_length=15)
     donation_request_user = models.CharField(blank=True,default=0,max_length=30)
+
 
     def __str__(self):
         return self.donation_amount
