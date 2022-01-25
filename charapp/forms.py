30935b32dce09f475ce9d6cd889d5c1c81541d00
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-25 08:01:30 +0300

    update auth

--- charapp/forms.py
+++ charapp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import NGO
+from django.contrib.auth.models import User
+from .models import Profile
 
 class NGO_form(forms.ModelForm):
     class Meta:
@@ -7,3 +9,16 @@ class NGO_form(forms.ModelForm):
         fields = ('ngo_name','head_of_ngo','contactNo','email')
 
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
