6d86aa8b0d9265b419e8a8a526f355ead52adb53
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-25 08:02:04 +0300

    update ngo profile

--- charapp/models.py
+++ charapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from PIL import Image
+from django.contrib.auth.models import User
 
 # Create your models here.
 class NGO(models.Model):
@@ -7,3 +9,20 @@ class NGO(models.Model):
     head_of_ngo = models.CharField(max_length=30,blank=True)
     contactNo = models.CharField(max_length=10,blank=True)
     email = models.EmailField(blank=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
