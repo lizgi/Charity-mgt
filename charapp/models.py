9d5cbf2b8dfac640108a7cb65614d4753a9d1eb8
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-26 08:53:11 +0300

    update ngo models

--- charapp/models.py
+++ charapp/models.py
@@ -8,20 +8,37 @@ from django_currentuser.middleware import get_current_user, get_current_authenti
 
 class donation_request(models.Model):
 
-    donation_description = models.TextField(blank=True,default=None)
+    donation_description = models.TextField(blank=True,default=0)
     donation_amount = models.CharField(default=None,blank=True,max_length=15)
-    donation_request_user = models.CharField(blank=True,default=get_current_authenticated_user,max_length=30)
+    donation_request_user = models.CharField(blank=True,default=0,max_length=30)
 
     def __str__(self):
         return self.donation_amount
       
 class NGO(models.Model):
     ngo_name = models.CharField(max_length=30,blank=True)
-    domain = models.CharField(max_length=20,blank=True)
     head_of_ngo = models.CharField(max_length=30,blank=True)
     contactNo = models.CharField(max_length=10,blank=True)
     email = models.EmailField(blank=True)
+    Amount = models.CharField(max_length=30,blank=True)
+    Reason_for_donation= models.CharField(max_length=30,blank=True)
+    verification_status = models.NullBooleanField(default=None,blank=True,null=True)
+    ngo_current_user = models.CharField(default=0,blank=True,max_length=40)
 
+    def verification_true(self):
+        self.verification_status=True        
+        self.save()
+
+    def verification_false(self):
+        self.verification_status = False
+        self.save()
+
+    def get_user(self,user):
+        self.ngo_user = user
+        self.save()
+
+    def _str_(self):
+        return self.ngo_name
 class Profile(models.Model):
     user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
