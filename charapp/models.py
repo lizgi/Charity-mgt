efbbb2047ecfddd6adf09c5e66bed63f567a604a
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2022-01-26 11:38:05 +0300

    Create category model, modify other models

--- charapp/models.py
+++ charapp/models.py
@@ -1,3 +1,4 @@
+from asyncio.base_futures import _PENDING
 from distutils.command.upload import upload
 from email.mime import image
 from email.policy import default
@@ -20,40 +21,17 @@ STATUS_CHOICES = [
     ('A', 'Approved'),
     ('w', 'Withdrawn'),
 ]
-
-class donation_request(models.Model):
-    
-    donation_description = models.TextField(blank=True,default=0)
-    donation_amount = models.CharField(default=0,blank=True,max_length=15)
-    donation_request_user = models.CharField(blank=True,default=0,max_length=30)
-
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES,default=_PENDING)
-    admin_approved = models.BooleanField(default=False)
-    date_posted = models.DateTimeField(auto_now_add=True,null=True)
+  
+#Category class
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.donation_amount
+        return self.name
 
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    image = models.ImageField(default = 'default.jpg', upload_to='profile_pics')
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-      
 class NGO(models.Model):
     ngo_name = models.CharField(max_length=30,blank=True)
     head_of_ngo = models.CharField(max_length=30,blank=True)
@@ -63,6 +41,7 @@ class NGO(models.Model):
     Reason_for_donation= models.CharField(max_length=30,blank=True)
     verification_status = models.NullBooleanField(default=0,blank=True,null=True)
     ngo_current_user = models.CharField(default=0,blank=True,max_length=40)
+
 
     def verification_true(self):
         self.verification_status=True        
@@ -99,11 +78,18 @@ class Profile(models.Model):
 
 
 class donation_request(models.Model):
-    ngo_name = models.CharField(default=0,max_length=50,blank=False,primary_key=True)
-    donation_description = models.TextField(blank=True,default=None)
+    ngo_name = models.CharField(max_length=50,blank=False,primary_key=True)
+    head_of_ngo = models.CharField(max_length=30,blank=True)
+    contactNo = models.CharField(max_length=10,blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    email = models.EmailField(blank=False)
     donation_amount = models.CharField(default=0,blank=True,max_length=15)
-    donation_request_user = models.CharField(blank=True,default=0,max_length=30)
-    Request_for_donation = models.CharField(blank=True,default=0,max_length=30)
+    donation_request_user = models.CharField(blank=True,max_length=30)
+    Reason_for_donation_request = models.CharField(blank=True,max_length=500)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES,default=_PENDING)
+    admin_approved = models.BooleanField(default=False)
+
+
 
 
     def __str__(self):
@@ -123,4 +109,4 @@ class donation_request_view(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'post_request'  
+        db_table = 'post_request' 
