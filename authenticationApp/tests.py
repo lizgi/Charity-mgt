4ff6e6c8745bffccd18f60c5c6aa6aa018ea116e
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-02-01 08:48:10 +0300

    add tests

--- authenticationApp/tests.py
+++ authenticationApp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import unittest
+from models import Donors
 
 # Create your tests here.
+class TestDonors(unittest.TestCase):
+    def setUp(self):
+        self.new_user = Donors('Timothy', '9000T')
+        
+        
+    def test_init(self):
+        self.assertEqual(self.new_user.username, 'Timothy')
+        self.assertEqual(self.new_user.password, '9000T')
+    
+    def test_save_donors(self):
+        self.new_user.save_user()
+        self.assertEqual(len(Donors.user_list), 1)
