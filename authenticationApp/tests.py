2eaf4da255876d8c0137fae226298dd46111bfdf
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2022-02-01 23:46:34 +0300

    Register ngo and donor, add a testcase

--- authenticationApp/tests.py
+++ authenticationApp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="test_user")
+        
