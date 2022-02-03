c85b4fc5eea2ebb017741617a86c3c6434e9564a
hassan3111 <ali.hassan@student.moringaschool.com>
2022-02-03 08:44:32 +0300

    add deployment

--- charproject/settings.py
+++ charproject/settings.py
@@ -161,6 +161,10 @@ SECURE_SSL_REDIRECT = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
